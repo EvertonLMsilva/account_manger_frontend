26427c8aa89c7659ca17b6dd528c93fd9cf5d92f
Everton Moraes <everton.moraes@ibstecnologia.com.br>
2021-05-22 13:44:44 -0300

    criação dos arquivos para tradução

---
+++ src/translate/languages/pt.js
@@ -0,0 +1,21 @@
+const messages = {
+  pt: {
+    translations: {
+          login:{
+              button:{
+                  facebook: 'CONTINUAR COM O FACEBOOK',
+                  github: 'CONTINUAR COM O GITHUB',
+                  start: 'Entrar'
+              },
+              imput:{
+                  user: 'Email',
+                  password: 'Senha'
+              },
+              forgot: 'Ou '
+
+
+          }
+      }
+  },
+};
+export { messages };
